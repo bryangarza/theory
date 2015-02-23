758466221e2778510be3c91a7cde94f62604481d
Bryan Garza <brygarza@gmail.com>
2015-02-22 19:18:41 -0800

    Notes on Week 2

--- penn.hs
+++ penn.hs
@@ -5,6 +5,10 @@ import GHC.List (reverse, zipWith)
 import Data.Char (digitToInt)
 import GHC.Real (fromIntegral)
 
+-- Note: Examples from LYAH and RWH are included in this source file as well.
+
+-- Week 1
+
 square :: Integral a => a -> a
 square x = x * x
 
@@ -71,3 +75,134 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 1 a b c = [(a, b)]
 hanoi n a b c = hanoi (pred n) a c b ++ [(a, b)] ++ hanoi (pred n) c b a
+
+
+-- Week 2
+
+data BookInfo = Book Int String [String]
+                deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+
+-- better:
+
+-- type synonyms
+type CustomerID = Int
+type ReviewBody = String
+
+-- Value constructor has the same name as its type constructor.
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
+
+
+-- Algebraic data types
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+-- 3 ways to bill customers
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                   | CashOnDelivery
+                   | Invoice CustomerID
+                     deriving (Show)
+
+-- Differentiating between identical info used for different things:
+
+-- x and y coordinates or lengths
+data Cartesian2D = Cartesian2D Double Double
+                   deriving (Eq, Show)
+
+-- Angle and distance (magnitude)
+data Polar2D = Polar2D Double Double
+               deriving (Eq, Show)
+
+-- Record syntax
+data Customer = Customer {
+  customerID      :: Int,
+  customerName    :: String,
+  customerAddress :: Address
+  } deriving (Show)
+
+customer1 = Customer 271828 "J.R. Hacker"
+            ["255 Syntax Ct",
+             "Milpitas, CA 95314",
+             "USA"]
+
+customer2 = Customer {
+  customerID      = 271828,
+  customerAddress = ["1048576 Disk Drive",
+                     "Milpitas, CA 95314",
+                     "USA"],
+  customerName    = "Jane Q. Citizen"
+  }
+
+
+-- Parametrized types
+
+-- for example (defined in Prelude, Data.Maybe):
+-- data Maybe a = Nothing | Just a
+
+-- "We usually use type parameters when the type that's contained inside
+-- the data type's various value constructors isn't really that important
+-- for the type to work. A list of stuff is a list of stuff and it
+-- doesn't matter what the type of that stuff is, it can still work. If
+-- we want to sum a list of numbers, we can specify later in the summing
+-- function that we specifically want a list of numbers. Same goes for
+-- Maybe. Maybe represents an option of either having nothing or having
+-- one of something. It doesn't matter what the type of that something
+-- is." - http://learnyouahaskell.com/making-our-own-types-and-typeclasses
+
+-- Recursive types
+
+data List a = Cons a (List a)
+            | Nil
+              deriving (Show)
+
+-- Our List type and the built-in [a] are isomorphic.
+
+fromList (x:xs) = Cons x (fromList xs)
+fromList []     = Nil
+
+toList (Cons x xs) = x : toList xs
+toList Nil = []
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
+firstTree = Node "parent" Empty (Node "right child" Empty Empty)
+
+
+data NewTree a = NewNode a (Maybe (NewTree a)) (Maybe (NewTree a))
+            deriving (Show)
+simpleTree = NewNode "parent" Nothing (Just (NewNode "right" Nothing Nothing))
+
+-- Reporting errors
+
+mySecond :: [a] -> a
+mySecond xs = if null (tail xs)
+              then error "list too short"
+              else head (tail xs)
+
+safeSecond :: [a] -> Maybe a
+safeSecond [] = Nothing
+safeSecond xs = if null (tail xs)
+                then Nothing
+                else Just (head (tail xs))
+
+tidySecond :: [a] -> Maybe a
+tidySecond (_:x:_) = Just x
+tidySecond _       = Nothing
+
+
+lend3 amount balance
+  | amount <= 0             = Nothing
+  | amount > reserve  * 0.5 = Nothing
+  | otherwise               = Just newBalance
+  where reserve    = 100
+        newBalance = balance - amount

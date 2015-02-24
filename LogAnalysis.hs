d4edf54ae7124175a1550567cb3da14e4b2e86f7
Bryan Garza <brygarza@gmail.com>
2015-02-23 23:06:12 -0800

    Wrote part of exercise 2.1

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -2,3 +2,17 @@
 module LogAnalysis where
 
 import Log
+
+-- Exercise 2.1
+
+parseMessage :: String -> LogMessage
+parseMessage s =
+  case xs of
+   ("I":a:bs)   -> LogMessage Info (read a) (unwords bs)
+   ("W":a:bs)   -> LogMessage Warning (read a) (unwords bs)
+   ("E":a:b:cs) -> LogMessage (Error $ read a) (read b) (unwords cs)
+   _            -> Unknown s
+  where xs = words s
+
+parse :: String -> [LogMessage]
+parse xs = map (parseMessage) (lines xs)

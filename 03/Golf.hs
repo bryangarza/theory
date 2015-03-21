d5e3522d34cb61c859c4df76422132e5966576f0
Bryan Garza <brygarza@gmail.com>
2015-03-20 22:44:27 -0700

    2/3 exercises on hw 3

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,1 +1,16 @@
 module Golf where
+
+skips :: [a] -> [[a]]
+skips [] = []
+skips all@(_:xs) = all : (skips xs)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima = lm []
+
+lm :: Ord a => [a] -> [a] -> [a]
+lm maxs (a:b:c:xs)
+  | b > a && b > c = lm (maxs++[b]) (b:c:xs)
+  | otherwise = lm maxs (b:c:xs)
+lm maxs _ = maxs
+
+histogram :: [Integer] -> String

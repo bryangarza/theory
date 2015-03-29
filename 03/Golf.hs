1ea1e7f89bc8bdf0abd4bdae5df4454c26c8105e
Bryan Garza <brygarza@gmail.com>
2015-03-28 23:17:49 -0700

    Finish homework 3

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,4 +1,7 @@
 module Golf where
+import Control.Lens
+import Data.List (sort)
+import Data.Bool (bool)
 
 skips :: [a] -> [[a]]
 skips [] = []
@@ -14,3 +17,12 @@ lm maxs (a:b:c:xs)
 lm maxs _ = maxs
 
 histogram :: [Integer] -> String
+histogram xs = hist (sort xs) 0 0 0 ["          \n"]
+
+hist :: [Integer] -> Int -> Int -> Int -> [String] -> String
+hist [] _ _ _ out = concat $ reverse $ "==========\n0123456789\n" : out
+hist (x:xs) h cur prev out =
+  let xint = fromIntegral x
+      hn   = bool (h + 1) 0 $ prev < xint
+      outn = bool out (out ++ ["          \n"]) $ prev == xint
+  in hist xs hn cur xint (outn & ix hn . ix xint .~ '*')

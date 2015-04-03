5ed36243c8358a06f6f3d8f746c6f90e12f6aa21
Bryan Garza <brygarza@gmail.com>
2015-04-02 21:02:04 -0700

    complete 1.1

---
+++ 04/Hwk4.hs
@@ -0,0 +1,17 @@
+module Hw4 where
+
+-- Rewrite using *wholemeal programming* practices
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = foldr (\x -> (*) (x - 2)) 1 . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)

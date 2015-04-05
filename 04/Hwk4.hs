e3f40067c6aaf2e6cd884f3dc982d74674703190
Bryan Garza <brygarza@gmail.com>
2015-04-04 18:10:24 -0700

    1.2

--- 04/Hwk4.hs
+++ 04/Hwk4.hs
@@ -15,3 +15,9 @@ fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n | even n    = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/= 1) . iterate iter
+  where iter n
+          | even n    = n `div` 2
+          | otherwise = 3 * n + 1

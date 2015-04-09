4a36f6c9972eabbeb0008877ad6509a68e9b498d
Bryan Garza <brygarza@gmail.com>
2015-04-08 19:52:08 -0700

    Almost finish 5.3

--- cis194/05/Calc.hs
+++ cis194/05/Calc.hs
@@ -1,6 +1,7 @@
 module Calc where
 import ExprT
 import Parser
+import Data.Bool (bool)
 
 -- ex 1
 eval :: ExprT -> Integer
@@ -25,3 +26,35 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = min
+  mul = max
+
+instance Expr Mod7 where
+  lit = flip mod 7
+  add = (+)
+  mul = (*)
+
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7

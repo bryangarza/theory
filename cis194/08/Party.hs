def8773981e0f0794847c47677c0ba5462ddd362
Bryan Garza <brygarza@gmail.com>
2015-04-25 16:00:35 -0700

    Finish homework 8

--- cis194/08/Party.hs
+++ cis194/08/Party.hs
@@ -1,5 +1,6 @@
 module Party where
 
+import Data.List (intercalate)
 import Data.Tree
 import Employee
 
@@ -12,9 +13,24 @@ instance Monoid GuestList where
   (GL es1 fun1) `mappend` (GL es2 fun2) = GL (es1 ++ es2) $ fun1 + fun2
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun x@(GL _ funx) y@(GL _ funy)
-  | funx > funy = x
-  | otherwise   = y
+moreFun = max
 
-treeFold :: (a -> b -> b) -> b -> Tree a -> b
-treeFold f acc t = foldr f acc (flatten t)
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node a ts) = f a $ map (treeFold f) ts
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e gs = (w, wo) where
+  w  = glCons e (mconcat $ map snd gs)
+  wo = mconcat $ map (uncurry moreFun) gs
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry max . treeFold nextLevel
+
+funListFormat :: GuestList -> String
+funListFormat (GL es fun) = unlines $ ("Fun level: " ++ show fun) : l
+  where l = map empName es
+
+main :: IO ()
+main = do
+  t <- readFile "company.txt"
+  putStrLn $ funListFormat $ maxFun $ read t
